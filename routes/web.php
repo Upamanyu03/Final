766fae96827e82ec04ee64d1c89459d8bc45ba00
upamanyu01 <upamanyu@gmail.com>
2023-12-30 11:41:27 +0530

    little changes

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::post('/productupdate/{id}',[ProductController::class,('productupdate')]);
 Route::get('/addlabour',[LabourController::class,('labour')]);
 Route::POST('/addlabour',[LabourController::class,('addlabour')]);
 Route::get('/viewlabour',[LabourController::class,('labourview')]);
-Route::get('/del/{id}',[LabourController::class,('destroy')]);
+Route::get('/labordel/{id}',[LabourController::class,('destroy')]);
 Route::get('/edit1/{id}',[LabourController::class,('edit1')]);
 Route::post('/update1/{id}',[LabourController::class,('update1')]);
 Route::get('/newinvoice/{id}',[DemoController::class,('invoice')]);
