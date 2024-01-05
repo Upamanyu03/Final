0f6214f768612a6bba13c59f8dd6655b592efeb6
upamanyu01 <upamanyu@gmail.com>
2024-01-05 17:31:00 +0530

    update

--- resources/views/Invoice.blade.php
+++ resources/views/Invoice.blade.php
@@ -56,15 +56,18 @@
                 <textarea class="float-right" style="width: 80%" rows="2" name="observation"></textarea><br><br><br>
                 <form action="" method="">
                 <label for="" class=""> Search Product: </label>
-                <input type="search" id="name" name="search" class="float-right p-2"  style="width: 80% "><br><br>
+                <input type="search" id="search" name="search" class="float-right p-2"  style="width: 80% "><br><br>
                 </form>
             </form>
         </div>
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script>
+      $(document).ready(function(){
+        var query=$(this)
+      })
   </body>
 </html>
